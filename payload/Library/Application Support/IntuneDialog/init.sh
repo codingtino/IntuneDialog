a25df8f4b859593eeb0b317fb02dfc24ed6d95a6
t.naumann <t.naumann@gge-germany.de>
2025-06-11 11:23:13 +0200

    .

--- payload/Library/Application Support/IntuneDialog/init.sh
+++ payload/Library/Application Support/IntuneDialog/init.sh
@@ -160,8 +160,10 @@ monitor_app() {
   local retries=0
   init_logging "$app_log"
 
-  IFS=';' read -r -a start_array <<<"$(echo "$start_patterns" | sed 's/^;*//;s/;*$//')"
-  IFS=';' read -r -a success_array <<<"$(echo "$success_patterns" | sed 's/^;*//;s/;*$//')"
+  #  IFS=';' read -r -a start_array <<<"$(echo "$start_patterns" | sed 's/^;*//;s/;*$//')"
+  #  IFS=';' read -r -a success_array <<<"$(echo "$success_patterns" | sed 's/^;*//;s/;*$//')"
+  readarray -d ';' -t start_array < <(printf '%s;' "$start_patterns")
+  readarray -d ';' -t success_array < <(printf '%s;' "$success_patterns")
 
   {
     log "info" "Monitoring installation of $app_name..."
@@ -170,7 +172,7 @@ monitor_app() {
       # Success pattern check
       local success_hits=0
       for pattern in "${success_array[@]}"; do
-        if grep -Fq "$pattern" "$INSTALL_LOG"; then
+        if grep -Eq "$pattern" "$INSTALL_LOG"; then
           ((success_hits++))
           [[ "$success_mode" == "any" ]] && break
         elif [[ "$success_mode" == "all" ]]; then
@@ -189,7 +191,7 @@ monitor_app() {
       # Start pattern check
       if ! $start_detected; then
         for pattern in "${start_array[@]}"; do
-          if grep -Fq "$pattern" "$INSTALL_LOG"; then
+          if grep -Eq "$pattern" "$INSTALL_LOG"; then
             start_detected=true
             log "info" "Detected start of $app_name installation (pattern: $pattern)"
             echo "listitem: title: $app_name, status: wait, statustext: Installing..." >>"$COMMAND_FILE"
